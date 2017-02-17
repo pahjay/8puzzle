8b25179f4adf0286eb6d19ef4c8995f246230e49
Ryan Page <rpage101@gmail.com>
2017-02-17 11:24:47 -0800

    created API's, need implementation

---
+++ src/Board.java
@@ -0,0 +1,38 @@
+/**
+ * Created by ryan on 2/17/17.
+ */
+public class Board {
+    public Board(int[][] blocks){ // construct a board from an n-by-n array of blocks
+
+    }
+
+    public int dimension() { // board dimension n
+
+    }
+
+    public int manhatten() { // sum of Manhatten distances between blocks and goal
+
+    }
+
+    public boolean isGoal() { // is this board the goal board?
+
+    }
+
+    public Board twin(){ // a board that is obtained by exchanging any pair of blocks
+
+    }
+
+    public boolean equals(Object y){ // does this board equal y?
+
+    }
+
+    public Iterable<Board> nerightbors() { // all neighboring boards
+
+    }
+
+    public String toString(){ // string representation of this board (in the output format specified below)
+
+    }
+
+    public static void main(){ }
+}
